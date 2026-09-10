26db0c223d9b3a762e704e303c36ae22f9d8ec6e
strangerthingsev3r-ux <strangerthingsev3r@gmail.com>
2026-09-10 09:08:27 +0300

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,7 +25,7 @@ export const botConfig = {
       {
         name: "Jοιn Τhε Dιscοrd Sεrvεr : https://discord.gg/yjeEUQPmCy ", // required by Discord API, not shown in the client
         state: "Mαde By Odys",     // this is what people actually see
-        type: 1,               // Custom
+        type: 4,               // Custom
       },
     ],
   },
